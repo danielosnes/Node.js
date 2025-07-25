b49064a1c02cf1056690e0935e4ab27426fd2130
danielosnes <osnesdaniel@gmail.com>
2025-07-25 20:25:19 +0000

    add 7-reviewRouters.js to learn express routers

---
+++ Back-End Development/Learn Express Routers/7-reviewRouters.js
@@ -0,0 +1,5 @@
+/*
+Great Job - you created a much more readable and clean code base using [Express.Router()]
+Writing clean and readable code is one of the most important skills as a developer.
+This will make adding new features much easier as your APIs expand!
+*/
